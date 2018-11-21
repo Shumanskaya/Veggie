00a62e718aeee0b3ea0d8c814e9551ffee87d0ab
Evgenia <myemail@mail.ru>
2018-11-21 13:07:20 +0300

    adding Veggies files

---
+++ main.js
@@ -0,0 +1,364 @@
+var starters = [
+    {
+        "name": "Chilled tomato soup",
+        "list": "with mozzarella, basil & aged balsamic",
+        "price": "9.75"
+    },
+    {
+        "name": "Heritage Beetroot salad",
+        "list": "with Graceburn feta, chickweed, mint & smoked almonds",
+        "price": "12.75"
+    },
+    {
+        "name": "Burrata",
+        "list": "shaved artichoke gremolata, seeded cracker & basil",
+        "price": "13.75"
+    },
+    {
+        "name": "Summer squash risotto",
+        "list": "with sage, toasted pepita seeds & parmesan",
+        "price": "15.00"
+    },
+    {
+        "name": "Ruby salad",
+        "list": "with feta, mint & pomegranate",
+        "price": "11.50"
+    },
+    {
+        "name": "Spinach & ricotta tortelloni",
+        "list": "with salsa fresca, basil & pine nuts   ",
+        "price": "14.75"
+    },
+    {
+        "name": "Chilled tomato soup",
+        "list": "with mozzarella, basil & aged balsamic",
+        "price": "9.75"
+    },
+    {
+        "name": "Heritage Beetroot salad",
+        "list": "with Graceburn feta, chickweed, mint & smoked almonds",
+        "price": "12.75"
+    },
+    {
+        "name": "Burrata",
+        "list": "shaved artichoke gremolata, seeded cracker & basil",
+        "price": "13.75"
+    },
+    {
+        "name": "Summer squash risotto",
+        "list": "with sage, toasted pepita seeds & parmesan",
+        "price": "15.00"
+    },
+    {
+        "name": "Ruby salad",
+        "list": "with feta, mint & pomegranate",
+        "price": "11.50"
+    },
+    {
+        "name": "Spinach & ricotta tortelloni",
+        "list": "with salsa fresca, basil & pine nuts",
+        "price": "14.75"
+    }
+]
+var mainDishes = [
+    {
+        "name": "Chickpea hummus",
+        "list": "ground lamb, cumin & pinenuts",
+        "price": "17.35"
+    },
+    {
+        "name": "Italian chopped salad",
+        "list": "with wild mushrooms, peas & basil",
+        "price": "12.15"
+    },
+    {
+        "name": "Spaghetti",
+        "list": "with provolone, chorizo and olives",
+        "price": "13.75"
+    },
+    {
+        "name": "Cornish hake",
+        "list": "chorizo sauce, scorched tomatoes & padrons",
+        "price": "10.45"
+    },
+    {
+        "name": "Breast of chicken",
+        "list": "grilled courgette salad & spiced green chutney",
+        "price": "16.25"
+    },
+    {
+        "name": "Plum & hazelnut torte",
+        "list": "with clotted cream",
+        "price": "6.95"
+    },
+    {
+        "name": "Morbier",
+        "list": "with black grapes",
+        "price": "5.75"
+    },
+    {
+        "name": "Chickpea hummus",
+        "list": "ground lamb, cumin & pinenuts",
+        "price": "17.35"
+    },
+    {
+        "name": "Italian chopped salad",
+        "list": "with wild mushrooms, peas & basil",
+        "price": "12.15"
+    },
+    {
+        "name": "Spaghetti",
+        "list": "with provolone, chorizo and olives",
+        "price": "13.75"
+    },
+    {
+        "name": "Cornish hake",
+        "list": "chorizo sauce, scorched tomatoes & padrons",
+        "price": "10.45"
+    },
+    {
+        "name": "Breast of chicken",
+        "list": "grilled courgette salad & spiced green chutney",
+        "price": "16.25"
+    },
+    {
+        "name": "Plum & hazelnut torte",
+        "list": "with clotted cream",
+        "price": "6.95"
+    },
+    {
+        "name": "Morbier",
+        "list": "with black grapes",
+        "price": "5.75"
+    }
+]
+var desserts = [
+    {
+        "name": "Vanilla crème brûlée",
+        "list": "with blackberries",
+        "price": "9.75"
+    },
+    {
+        "name": "Warm brioche",
+        "list": "raspberries, ripple ice cream",
+        "price": "9.50"
+    },
+    {
+        "name": "Baked Alaska for two",
+        "list": "with griottines",
+        "price": "18.00"
+    },
+    {
+        "name": "Frosted buttermilk waffle",
+        "list": "blueberries & clotted cream",
+        "price": "8.25"
+    },
+    {
+        "name": "Cru virunga chocolate fondant",
+        "list": "with coconut ice cream",
+        "price": "9.50"
+    },
+    {
+        "name": "Summer fruit Pavlova",
+        "list": "with crème Chantilly",
+        "price": "9.75"
+    },
+    {
+        "name": "Seasonal fruits",
+        "list": "with sorbet",
+        "price": "11.00"
+    },
+    {
+        "name": "Vanilla crème brûlée",
+        "list": "with blackberries",
+        "price": "9.75"
+    },
+    {
+        "name": "Warm brioche",
+        "list": "raspberries, ripple ice cream",
+        "price": "9.50"
+    },
+    {
+        "name": "Baked Alaska for two",
+        "list": "with griottines",
+        "price": "18.00"
+    },
+    {
+        "name": "Frosted buttermilk waffle",
+        "list": "blueberries & clotted cream",
+        "price": "8.25"
+    },
+    {
+        "name": "Cru virunga chocolate fondant",
+        "list": "with coconut ice cream",
+        "price": "9.50"
+    },
+    {
+        "name": "Summer fruit Pavlova",
+        "list": "with crème Chantilly",
+        "price": "9.75"
+    },
+    {
+        "name": "Seasonal fruits",
+        "list": "with sorbet",
+        "price": "11.00"
+    }
+]
+var drinks = [
+    {
+        "name": "Fish House Punch",
+        "list": "Havana Club 3yrs, Rémy Martin VSOP, lime, peach, camomile tea",
+        "price": "11.50"
+    },
+    {
+        "name": "Clover Club",
+        "list": "Tanqueray No. 10, raspberry, lemon, egg white, Martini Extra Dry",
+        "price": "12.50"
+    },
+    {
+        "name": "Five Points",
+        "list": "Redbreast 12yrs, Bulleit Rye, Cocchi Americano, maraschino, Orinoco bitters",
+        "price": "11.50"
+    },
+    {
+        "name": "Carminetta Fizz",
+        "list": "The Ivy Centenary Gin, blackberry & pine shrub, lemon, ginger ale",
+        "price": "13.00"
+    },
+    {
+        "name": "Apple - tini",
+        "list": "Wyborowa vodka, green apple sherbet, lemon, fino",
+        "price": "11.00"
+    },
+    {
+        "name": "Amaretto Sour",
+        "list": "Amaretto di Saronno, plum - infused sake, Martell VSOP, cherry brandy, lemon, egg white",
+        "price": "10.00"
+    },
+    {
+        "name": "Burnt Butter Sidecar",
+        "list": "Beurre Noisette Martell VSOP, Grand Marnier, lemon, Sauternes",
+        "price": "11.50"
+    },
+    {
+        "name": "Fish House Punch",
+        "list": "Havana Club 3yrs, Rémy Martin VSOP, lime, peach, camomile tea",
+        "price": "11.50"
+    },
+    {
+        "name": "Clover Club",
+        "list": "Tanqueray No. 10, raspberry, lemon, egg white, Martini Extra Dry",
+        "price": "12.50"
+    },
+    {
+        "name": "Five Points",
+        "list": "Redbreast 12yrs, Bulleit Rye, Cocchi Americano, maraschino, Orinoco bitters",
+        "price": "11.50"
+    },
+    {
+        "name": "Carminetta Fizz",
+        "list": "The Ivy Centenary Gin, blackberry & pine shrub, lemon, ginger ale",
+        "price": "13.00"
+    },
+    {
+        "name": "Apple - tini",
+        "list": "Wyborowa vodka, green apple sherbet, lemon, fino",
+        "price": "11.00"
+    },
+    {
+        "name": "Amaretto Sour",
+        "list": "Amaretto di Saronno, plum - infused sake, Martell VSOP, cherry brandy, lemon, egg white",
+        "price": "10.00"
+    },
+    {
+        "name": "Burnt Butter Sidecar",
+        "list": "Beurre Noisette Martell VSOP, Grand Marnier, lemon, Sauternes",
+        "price": "11.50"
+    }
+]
+
+
+function addMenu(menu) {
+    for (let i in menu) {
+        let name = menu[i].name
+        let price = menu[i].price
+        let list = menu[i].list
+        if (i < (menu.length / 2)) {
+            addMenuFirstColumn(name, price, list)
+        }
+        else {
+            addMenusecondColumn(name, price, list)
+        }
+    }
+}
+
+function emptyMenu() {
+    $('.test1').empty('<div class="flexMenu"><div class="name"><p class="text-uppercase"></p></div><div class="dots"></div><div class="price"><p></p></div></div><div class="textMenu"></div>')
+    $('.test2').empty('<div class="flexMenu"><div class="name"><p class="text-uppercase"></p></div><div class="dots"></div><div class="price"><p></p></div></div><div class="textMenu"></div>')
+}
+
+function addMenuFirstColumn(name, price, list) {
+    $('.test1').append('<div class="flexMenu"><div class="name"><p class="text-uppercase">' + name + '</p></div><div class="dots"></div><div class="price"><p>' + price + '</p></div></div><div class="textMenu">' + list + '</div>')
+}
+function addMenusecondColumn(name, price, list) {
+    $('.test2').append('<div class="flexMenu"><div class="name"><p class="text-uppercase">' + name + '</p></div><div class="dots"></div><div class="price"><p>' + price + '</p></div></div><div class="textMenu">' + list + '</div>')
+}
+
+function focusAndBlur(check) {
+    console.log(check)
+    $(check).focus(function () {
+        $(this).css('border', '1px solid black')
+    })
+    $(check).blur(function () {
+        $(this).css('border', 'none')
+    })
+}
+
+[
+    'starters',
+    'mainDishes',
+    'desserts',
+    'drinks'
+].forEach(function(i){
+    let selector = $('#' + i);
+    focusAndBlur(selector);
+    selector.click(function(sel){
+        emptyMenu();
+        addMenu(eval(i))
+    })
+});
+
+$(document).ready(function(){
+    $('#starters').click();
+})
+
+
+function photoBlockUp(i, timeout) {
+    let selector = '.section_spec_'
+    setTimeout(function () {
+        $(selector + 'photo' + i).addClass('up')
+    }, timeout * i);
+    setTimeout(function () {
+        $(selector + 'block' + i).addClass('up')
+    }, timeout * i + timeout * 6);
+}
+
+
+$(window).scroll(function () {
+    let w = $('body').scrollTop()
+    if (w >= 1100) {
+        for (let i = 0; i <= 6; i++) {
+            photoBlockUp(i, 150)
+        }
+        $('.backTotop').css('display', 'block')
+    }
+    else {
+        $('.backTotop').css('display', 'none')  
+    }
+})
+
+$('.backTotop').click(function() {
+    $('body').animate({
+        scrollTop: 0
+    }, 1000)
+})
